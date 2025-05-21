78eb95ae563b810432652f8baf711fa45603e138
Yezu Yi <80572363+js4484821266@users.noreply.github.com>
2025-05-21 20:42:57 +0900

    Title: 아름다운 수, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250521/아름다운 수/beautiful-number.py
+++ 250521/아름다운 수/beautiful-number.py
@@ -2,9 +2,9 @@ n = int(input())
 
 # Please write your code here.
 def f(k):
-    if k<=2:return k
+    if k<=1:return 1
     s=0
-    for i in range(min(4,k-1)):
+    for i in range(min(4,k)):
         s+=f(k-(i+1))
-    return s+1
+    return s
 print(f(n))
