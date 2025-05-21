40991856594959f385584d2346ad525b25798a46
Yezu Yi <80572363+js4484821266@users.noreply.github.com>
2025-05-21 09:25:06 +0900

    Title: 아름다운 수, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250521/아름다운 수/beautiful-number.py
+++ 250521/아름다운 수/beautiful-number.py
@@ -1,15 +1,10 @@
 n = int(input())
 
 # Please write your code here.
-l=[0 for i in range(n+1)]
-l[1]=1
 def f(k):
-    if k==0:return 0
-    elif l[k]>0:return l[k]
-    else:
-        s=0
-        for i in range(1,min(k+1,10)):
-            s+=f(k-i)
-        l[k]=s+1
-        return l[k]
+    if k<=1:return k
+    s=0
+    for i in range(1,k):
+        s+=f(i)
+    return s+1
 print(f(n))
